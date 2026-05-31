2530c4ef9563863baf78c4bebf9126fa59c7fade
Louisfy <54393156+louisfy@users.noreply.github.com>
2026-05-31 16:26:56 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AIChatInterface from '../AIChatInterface.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AIChatInterface />
+  </React.StrictMode>,
+)
